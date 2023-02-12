e0f0bc145bd8a7885f9db84ba6a1f42b129d5a0e
Karol Chilimoniuk <webdevkarolchilimoniuk@gmail.com>
2023-02-12 17:23:21 +0100

    feat: html content and JS logic added

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,51 @@
-import "../scss/main.scss";
+"use strict";
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+class App {
+  constructor() {
+    this.reposSection = document.querySelector(".section-recentProjects--js");
+  }
 
-/* place your code below */
+  startApp() {
+    fetch("https://api.github.com/users/karolchilimoniuk/repos?sort=updated")
+      .then((resp) => resp.json())
+      .then((resp) => {
+        const repos = resp;
+        for (let repo of repos) {
+          const { name, description, html_url, homepage } = repo;
+          if (
+            description &&
+            repo.private === false &&
+            repo.name !== "wtf-crew" &&
+            repo.name !== "weather-app-with-bcknd"
+          ) {
+            this.reposSection.innerHTML += `<section class="recentProjects__description">
+            <img class="recentProjects__description-img" src="assets/svg/github.svg" alt="github">
+            <h4 class="recentProjects__description-title">${name}</h4>
+            <p class="recentProjects__description-paragraph">${description}</p>
+            <div class = "recentProjects__demo">
+              <p class = "recentProjects__demo-paragraph" >
+                ${
+                  homepage &&
+                  `<a href="${homepage}" class="recentProjects__demoLink" target="_blank" title="Project's demo.">
+                  <img class="recentProjects__demo-img" src="assets/svg/comp.svg" alt="computer.">
+                   Demo
+                </a>`
+                }
+                <a href="${html_url}" class="recentProjects__codeLink" target="_blank" title="Project's git.">
+                  <img class="recentProjects__demo-img" src="assets/svg/code.svg"   alt="code.">
+                   Github
+                </a>
+              </p>
+            </div>
+          </section>`;
+          }
+        }
+      });
+    console.log(
+      "Hello! My name is Charlie. I am aspiring front-end developer and I am still looking for new opportunities to cooperation. Don't hesitate to ask me 😉"
+    );
+  }
+}
 
-console.log("HELLO 🚀");
+const app = new App();
+app.startApp();
