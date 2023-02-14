511977ee6887aab96f3d78d64d5be0bb9d10bc16
Karol Chilimoniuk <webdevkarolchilimoniuk@gmail.com>
2023-02-14 22:29:46 +0100

    feat: added icons to recentProjects section

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import "../scss/main.scss";
+import githubIcon from "../assets/icons/github.svg";
+import compIcon from "../assets/icons/comp.svg";
 
 ("use strict");
 
@@ -19,23 +21,27 @@ class App {
             repo.private === false &&
             repo.name !== "wtf-crew" &&
             repo.name !== "KarolChilimoniuk" &&
-            repo.name !== "weather-app-with-bcknd"
+            repo.name !== "weather-app-with-bcknd" &&
+            repo.name !== "KarolChilimoniuk.github.io"
           ) {
             this.reposSection.innerHTML += `<section class="recentProjects__description">
             <h4 class="recentProjects__description-title">${name}</h4>
             <p class="recentProjects__description-paragraph">${description}</p>
             <div class = "recentProjects__demo">
               <p class = "recentProjects__demo-paragraph" >
-                ${
-                  homepage &&
-                  `<a href="${homepage}" class="recentProjects__demoLink" target="_blank" title="Project's demo.">
-                  Demo
-                </a>`
-                }
-                <a href="${html_url}" class="recentProjects__codeLink" target="_blank" title="Project's git.">
-                   Github
-                </a>
-              </p>
+              <p class = "recentProjects__demo-paragraph" >
+              ${
+                homepage &&
+                `<a href="${homepage}" class="recentProjects__demoLink" target="_blank" title="Project's demo.">
+                <img class="recentProjects__demo-img" src="${compIcon}" alt="computer.">
+                 <span class="recentProjects__demo-span">Demo</span>
+              </a>`
+              }
+              <a href="${html_url}" class="recentProjects__codeLink" target="_blank" title="Project's git.">
+                <img class="recentProjects__demo-img" src="${githubIcon}"   alt="GitHub repo">
+                <span class="recentProjects__demo-span">Github</span>
+              </a>
+            </p>
             </div>
           </section>`;
           }
